8458787528405ad139d993ff4021b7f92ea48eee
cristian <cristiangodoyy@gmail.com>
2020-06-24 18:20:06 -0300

    generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -21,93 +21,23 @@ http POST http://127.0.0.1:8000/snippets/ code="public class Name { }"
 """
 
 
-class SnippetList(APIView):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
+    http://127.0.0.1:8008/snippets/
     """
     authentication_classes = [SessionAuthentication,
                               BasicAuthentication]
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
     #                       permissions.IsAuthenticated]
 
-    def get(self, request, format=None):
-        """
-        Obtiene una lista de Snippets: get  http://127.0.0.1:8008/snippets/
-        [{"owner":"admin","owner_id":1,"id":1,"title":"","code":"print(111)","linenos":true,"language":"python","style":"friendly"},{"owner":"admin","owner_id":1,"id":2,"title":"","code":"print(222)","linenos":false,"language":"python","style":"friendly"}]
-        :param request: 
-        :param format: 
-        :return: 
-        """
-        snippets = Snippet.objects.all()
-        serializer = SnippetSimpleSerializer(snippets, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        """
-        post http://127.0.0.1:8008/snippets/
-         {
-            "title": "",
-            "code": "print(456)",
-            "linenos": false,
-            "language": "python",
-            "style": "friendly"
-        }
-        :param request: 
-        :param format: 
-        :return: 
-        """
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(owner=self.request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(data=request.data, many=True)
-        if serializer.is_valid():
-            serializer.update(instance=snippets, validated_data=serializer.validated_data)
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
 
-class SnippetDetail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-    #                       IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        """
-        
-        :param request: 
-        :param pk: 
-        :param format: 
-        :return: 
-        """
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
 
 class UserList(generics.ListAPIView):
